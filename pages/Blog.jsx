41db2ecf743559374ebb3f2e490d0abb9606ef36
arcav <armandojcastro84@gmail.com>
2022-12-26 16:55:30 -0400

    feat:add sanity basic congif,utils and blog fetch

--- pages/Blog.jsx
+++ pages/Blog.jsx
@@ -1,30 +1,23 @@
-import { createClient } from "next-sanity";
-import Image from "next/image";
 import React from "react";
+import { client } from "../utils/sanityClient";
+import {urlFor} from "../utils/imgBuilder"
 
-const client = createClient({
-  projectId: "gylmzmsj",
-  dataset: "production",
-  apiVersion: "2022-03-25",
-  useCdn: false,
-  ignoreBrowserTokenWarning: true,
 
-});
 export async function getStaticProps() {
   const posts = await client.fetch(`*[_type == "post"]`);
-
+  console.log(urlFor(posts));
   return {
     props: { posts },
   };
 }
 const Blog = ({ posts }) => {
-  console.log(posts)
   return (
     <>
-      {posts.map((x, index) => (
-        <div key={index}>
-          <p>{x.title}</p>
-        </div>
+      {posts.map((post, i) => (
+        <>
+          <p>{post.title}</p>
+          <img src={urlFor(post.mainImage)} />
+        </>
       ))}
     </>
   );
