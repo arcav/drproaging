706e4d9763b5d818f027140dcbda97bb898d8806
arcav <armandojcastro84@gmail.com>
2023-01-31 16:23:41 -0400

    fix:Logo from new Layout

--- components/Logo/index.jsx
+++ components/Logo/index.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import React from "react";
-import Black from "../../public/Black.svg";
+import White from "../../public/Black.svg";
 
 export const Logo = () => {
   return (
-    <div className=" h-[30%] lg:w-[40%] lg:p-2 lg:m-12 mt-3 mx-4 ">
-      <Image src={Black} alt="logo" width={800} height={300} />
-    </div>
+    <>
+      <Image src={White} alt="logo" width={400} height={100} />
+    </>
   );
 };
