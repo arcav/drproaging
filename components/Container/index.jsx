f85facbeb580580fac03af400916dafe8c0022d1
arcav <armandojcastro84@gmail.com>
2023-02-01 19:43:53 -0400

    fix:Container fix styles ans positions element

--- components/Container/index.jsx
+++ components/Container/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className=" mx-auto  w-[95%] bg-white rounded-xl border border-slate-200 shadow-md shadow-slate-600">
-      {children}
+    <div className=" flex justify-center items-center m-auto my-5 h-screen w-[95%] bg-white rounded-xl border border-slate-200 shadow-md shadow-slate-600">
+      <h1>{children}</h1>
     </div>
   );
 };
