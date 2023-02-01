139146c2c4760358e3795db14ed58d0517ce279d
arcav <armandojcastro84@gmail.com>
2023-02-01 13:37:19 -0400

    feature:sidebar links implementation:

--- components/SideBar/SideBar.jsx
+++ components/SideBar/SideBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-
-export const SideBar = ({ openHandler, open }) => {
+export const SideBar = ({ openHandler, open, sideBarLink }) => {
   return (
     <div
-    className={`w-3/4 h-screen bg-[#010f24] bg-opacity-80 z-20 top-0 right-0 fixed transition-all duration-700  ${
-      open ? "left-0" : "-left-full"
-    }`}
-  >
+      className={`w-3/4 h-screen bg-[#010f24] bg-opacity-80 z-20 top-0 right-0 fixed transition-all duration-700  ${
+        open ? "left-0" : "-left-full"
+      }`}
+    >
       <div onClick={openHandler}>x</div>
+      <div className="flex flex-col">{sideBarLink}</div>
     </div>
   );
 };
