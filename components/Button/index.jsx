90e8675727aa634bbb75af3fce7222b21f8df0bc
arcav <armandojcastro84@gmail.com>
2023-02-01 19:44:45 -0400

    fix:Link props children for navigations

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-const Button = ({ children }) => {
+const Button = ({ children, href }) => {
   return (
     <div className="p-3 mx-auto">
-      <Link href={children}>
+      <Link href={href}>
         <button className="btn btn-wide rounded-md h-10 bg-black shadow-md shadow-black  text-white text-lg font-bold  border-double border-[1px] border-white hover:border-black  hover:bg-white  hover:text-black  hover:shadow-md hover:shadow-black hover:transition hover:ease-linear  delay-100  hover:-translate-y-1 hover:scale-100  duration-700 cursor-pointer">
           {children}
         </button>
