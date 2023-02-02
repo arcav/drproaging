21164a3bbda5d26bc2c5ae4072f7bad432dbcd74
arcav <armandojcastro84@gmail.com>
2023-02-01 21:25:17 -0400

    remove all sanity files

--- utils/sanityClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "gylmzmsj",
-  dataset: "production",
-  apiVersion: "2022-03-25",
-  useCdn: false,
-  ignoreBrowserTokenWarning: true,
-});
